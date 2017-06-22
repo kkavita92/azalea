dcbd9b1bb5e99c20cdd386f4b033b8626751f4a6
Kavita <kkavita92@gmail.com>
2017-06-23 00:06:03 +0100

    - Catch errors within #it function - Add sampleTests.js to test framework

--- azalea/azalea-core.js
+++ azalea/azalea-core.js
@@ -38,17 +38,6 @@ function expect(testvalue) {
     };
   };
 
-
-  function error(errorMessage) {
-    test.innerHTML += `<p class='fail'>${errorMessage}</p>`
-  };
-
-  function runErrorProcess() {
-    error(errorMessage);
-    var list = test.getElementsByTagName("LI")
-    list[list.length - 1].className = "fail";
-  };
-
   return {
     toEqual: toEqual,
     toNotEqual: toNotEqual,
@@ -56,4 +45,47 @@ function expect(testvalue) {
     toBeDefined: toBeDefined
   }
 
+};
+
+
+function error(errorMessage) {
+  test.innerHTML += `<p class='fail'>${errorMessage}</p>`
+};
+
+function runErrorProcess() {
+  error(errorMessage);
+  var list = test.getElementsByTagName("LI")
+  list[list.length - 1].className = "fail";
+};
+
+function describe(testHeading, func) {
+  document
+  .getElementById("test")
+  .innerHTML += `<h3>${testHeading}</h3>`;
+  func();
+  testCounter();
+};
+
+function it(description, block) {
+  try {
+    numberOfTests++;
+    document
+    .getElementById("test")
+    .innerHTML += `<li>${description}</li>`;
+    block();
+  } catch(error) {
+    failures++;
+    errorMessage = `- ${error.stack}`
+    runErrorProcess();
+  };
+};
+
+function testCounter() {
+  var fails;
+  var color;
+  failures === 1 ? fails = 'failure' : fails = 'failures';
+  failures ? color = 'red' : color = '#00cc00';
+  document
+  .getElementById("test-report")
+  .innerHTML = `<span style="color:${color}">${numberOfTests} tests, ${failures} ${fails}</span>`;
 };
---
+++ sampleTests.js
@@ -0,0 +1,6 @@
+describe('Sample Tests', function() {
+  it('can pick up definition errors', function() {
+    var thing = new Thing();
+    expect(thing).toBeDefined();
+  });
+});
